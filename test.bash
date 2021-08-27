f0d9b0d338e7dbeeecdf96cb689475a440f845ce
COMoER <zhengyu0730@sjtu.edu.cn>
2021-08-27 18:06:36 +0800

    debug test.bash extra $

--- test.bash
+++ test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-$root_path=~/DATA/Workspace/zy_stock/LCR_sjtu # change to your root path
-$livox_path=/home/sjtu/DATA/Workspace/zy_stock/yolov5_4.0_radar/lidar_area
+root_path=~/DATA/Workspace/zy_stock/LCR_sjtu # change to your root path
+livox_path=/home/sjtu/DATA/Workspace/zy_stock/yolov5_4.0_radar/lidar_area
 cd $root_path 
 gnome-terminal --geometry 60x20+10+10 -- bash $livox_path/start.sh
 gnome-terminal --geometry 60x20+10+10 -- bash main_l.sh
